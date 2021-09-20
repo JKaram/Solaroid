5f0776d17b85d095fe319e2b9f8500e7c5d1e33b
jamie <jamiekaram16@gmail.com>
2021-09-20 19:02:43 -0400

    style loading bar

--- components/commons/Empty.tsx
+++ components/commons/Empty.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Empty({ empty, error, status }) {
   return (
     <div className={`text-white ${empty ? "block" : "hidden"} flex flex-col items-center`}>
-      <Image src={"/empty.svg"} width="300" height="300" />
+      <Image alt="empty section" src={"/empty.svg"} width="300" height="300" />
       {empty && !error && status}
       {error && "Looks like we have an error"}
     </div>
--- components/commons/LoadingBar.tsx
+++ components/commons/LoadingBar.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Image from "next/image";
 
 export default function LoadingBar(props) {
   return (
-    <div className="w-1/2 h-11 bg-yellow-200 text-gray-800 m-auto mb-10 rounded" {...props.rest}>
-      Loading ...
+    <div
+      className="w-1/2 h-11 border-2 border-spacepink bg-spacepink bg-opacity-30 items-center justify-center flex m-auto mb-10 rounded py-4"
+      {...props.rest}
+    >
+      <Image className="animate-spin bg-transparent my-2" alt="spinner" src="/spinner.svg" height="30" width="30" />
+      Loading
     </div>
   );
 }
