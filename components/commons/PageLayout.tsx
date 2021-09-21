73b579b0970688f5240c8981450486c61e4f9872
jamie <jamiekaram16@gmail.com>
2021-09-20 20:15:43 -0400

    style nav

--- components/commons/PageLayout.tsx
+++ components/commons/PageLayout.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 import Nav from "../sections/Nav/Nav";
 
 interface Props {
@@ -6,9 +7,14 @@ interface Props {
 }
 
 export default function PageLayout({ children }: Props) {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const entry = useIntersectionObserver(ref, {});
+  const isVisible = entry?.isIntersecting;
+
   return (
     <div className="bg-hero-pattern text-white h-full min-h-screen pb-16">
-      <Nav />
+      <div ref={ref}></div>
+      <Nav isScroll={isVisible} />
       <main className="w-100 md:max-w-3xl m-auto">{children}</main>
     </div>
   );
--- components/sections/Nav/Nav.tsx
+++ components/sections/Nav/Nav.tsx
@@ -3,25 +3,31 @@ import Image from "next/image";
 import Link from "next/link";
 interface Props {
   //   children: React.ReactNode;
+  isScroll: boolean;
   rest?: any;
 }
 export default function Nav(props: Props) {
+  const { isScroll = true } = props;
   return (
     <nav
-      className="w-100 md:max-w-2xl m-auto flex justify-between align-middle items-center px-2 py-4  mb-4 text-white sticky top-0 z-50"
+      className={`w-100  m-auto flex justify-between align-middle items-end py-4 mb-4 text-white sticky top-0 z-50 transition-all  ${
+        !isScroll && "bg-spacedarkpurple shadow-2xl"
+      } `}
       {...props.rest}
     >
-      <div className="">
-        <Link href="/">
-          <div className="flex cursor-pointer items-center">
-            <Image src="/solaroid.svg" alt="logo" width="100%" height="45" />
-            <h1 className="hidden sm:block text-2xl m-0 font-bold">Solaroid</h1>
-          </div>
-        </Link>
+      <div className="m-auto w-full flex justify-between items-end md:max-w-3xl">
+        <div className="">
+          <Link href="/">
+            <div className="flex cursor-pointer items-end ">
+              <Image src="/solaroid.svg" alt="logo" width="100%" height="45" />
+              <h1 className="hidden sm:block text-2xl m-0 font-bold ">Solaroid</h1>
+            </div>
+          </Link>
+        </div>
+        <span className="text-xl text-white cursor-pointer border-b-2 border-spacepink hover:bg-spacepink hover:border-spacepink">
+          <Link href="/MyLiked">My Liked</Link>
+        </span>
       </div>
-      <Link href="/MyLiked">
-        <span className="text-xl text-white cursor-pointer hover:border-white hover:border-2">My Liked</span>
-      </Link>
     </nav>
   );
 }
