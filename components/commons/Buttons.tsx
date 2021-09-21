75e14960dc19d3fbe9a3dff2cd3f3e196af31829
jamie <jamiekaram16@gmail.com>
2021-09-20 21:11:04 -0400

    add copied animation

--- components/commons/Buttons.tsx
+++ components/commons/Buttons.tsx
@@ -1,31 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useAppState from "../../hooks/useAppState";
 import { HeartIcon, ClipboardCopyIcon } from "@heroicons/react/solid";
+import useCopyToClipboard from "../../hooks/useCopyToClipboard";
 
 export function LikeButton(props) {
   const { likes, addLike, removeLike } = useAppState();
   const { date } = props;
+  const isLiked = likes.includes(date);
   return (
     <button
       onClick={() => (likes.includes(date) ? removeLike(date) : addLike(date))}
-      className="flex items-center text-sm text-gray-200 px-1  rounded-md bg-white bg-opacity-25 hover:bg-opacity-50 active:bg-opacity-75 transition-all"
+      className={`flex items-center  rounded px-2 border-2 bg-spacepink border-spacepink bg-opacity-30 transition-all hover:bg-opacity-60 ${
+        isLiked && "bg-white border-white"
+      }`}
     >
-      <HeartIcon className="h-4 w-4 text-red-500" />
-      {likes.includes(date) ? "UnLike" : "Like"}
+      <HeartIcon className={`h-4 w-4 text-spacepink ${isLiked && "text-white"}`} />
+      {isLiked ? "Unlike" : "Like"}
     </button>
   );
 }
 
 export function ShareButton(props) {
   const { date } = props;
+  const [value, copy] = useCopyToClipboard();
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (copied) setCopied(false);
+    }, 1500);
+
+    return () => clearTimeout(timeout);
+  }, [copied]);
 
   return (
     <button
-      onClick={() => navigator.clipboard.writeText(`http://localhost:3000/share/${date}`)}
-      className="flex items-center text-gray-200 px-3 py-2 rounded-md bg-white bg-opacity-25 hover:bg-opacity-50 active:bg-opacity-75 transition-all text-white"
+      onClick={() => {
+        setCopied(true);
+        copy(`http://localhost:3000/share/${date}`);
+      }}
+      className="flex items-center"
     >
-      <ClipboardCopyIcon className="h-5 w-5 text-blue-500" />
-      Share Link
+      <ClipboardCopyIcon className={`h-5 w-5 ${copied ? "text-spacepink" : "text-white"}`} />
+      {copied && <span className="text-spacepink">Copied!</span>}
+      {!copied && <span>Share</span>}
     </button>
   );
 }
