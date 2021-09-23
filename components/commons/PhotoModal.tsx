e8231db57b6b461c678d0948fd64344827c17aeb
jamie <jamiekaram16@gmail.com>
2021-09-22 22:42:32 -0400

    fix modal spacing

--- components/commons/PhotoModal.tsx
+++ components/commons/PhotoModal.tsx
@@ -40,7 +40,7 @@ export default function PhotoModal({ img, onClose, showModal }: Props) {
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="relative bg-gradient-to-b from-spacedarkpurple via-spacedarkblue to-spacedarkblue text-white rounded max-w-4xl w-11/12 2xl:w-full  my-4">
+            <div className="relative shadow-xl bg-gradient-to-b from-spacedarkpurple via-spacedarkblue to-spacedarkblue text-white rounded max-w-4xl w-11/12 2xl:w-full  my-4">
               <XIcon
                 onClick={() => onClose()}
                 className="absolute right-1
@@ -51,12 +51,12 @@ export default function PhotoModal({ img, onClose, showModal }: Props) {
                   <figure className="relative h-96 my-3">
                     <Image alt={img.title} src={img.url} width="100%" height="100%" layout="fill" objectFit="contain" />
                   </figure>
-                  <div className="flex space-x-2 justify-center">
+                  <div className="flex space-x-2 mb-4 justify-center">
                     <LikeButton date={img.date} />
                     <ShareButton date={img.date} />
                   </div>
 
-                  <div className="px-2 pb-2">
+                  <div className="px-2 pb-2 w-full m-auto md:w-3/4 mb-2">
                     <h1 className="text-2xl font-bold mt-1">{img.title}</h1>
                     <h2 className="text-lg mb-2">{img.copyright}</h2>
                     <time>{format(addDays(new Date(img.date), 1), "MMMM do yyyy")}</time>
